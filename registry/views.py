7866947bd5ad1962935bc52bfdd1e3fccd751631
bibiana <bcristofol@gmail.com>
2018-05-10 10:50:40 +0100

    Get data by prefix

--- registry/views.py
+++ registry/views.py
@@ -8,11 +8,46 @@ app = Flask(
 )
 
 
+def get_prefix_data(data):
+    return {
+        'publisher': {
+            'name': data['publisher']['name'],
+            'logo': data['publisher']['logo']
+        },
+        'license': {
+            'url': data['license'],
+            'name': data['license_name']
+        },
+        'grant': []
+    }
+
+
+def get_grant_data(data):
+    return {
+        'url': data['distribution'][0]['downloadURL'],
+        'name': data['distribution'][0]['title']
+    }
+
+
+def get_data_by_prefix(raw_data):
+    data_by_prefix = {}
+
+    for data in raw_data:
+        prefix = data['publisher']['prefix']
+
+        if prefix not in data_by_prefix:
+            data_by_prefix[prefix] = get_prefix_data(data)
+
+        data_by_prefix[prefix]['grant'].append(get_grant_data(data))
+
+    return data_by_prefix
+
+
 @app.route('/data/data-registry')
 def data_registry():
-    data = requests.get('http://data.threesixtygiving.org/data.json').json()
+    raw_data = requests.get('http://data.threesixtygiving.org/data.json').json()
 
     return render_template(
         'registry.html',
-        data=data
+        data=get_data_by_prefix(raw_data)
     )
