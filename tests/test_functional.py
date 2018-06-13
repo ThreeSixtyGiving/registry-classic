f145aae051cfeded5fa8b037e840fdd324f74a6b
Bibiana Cristofol Amat <bcristofol@gmail.com>
2018-06-13 09:59:24 +0100

    Functional tests setup - not working

---
+++ tests/test_functional.py
@@ -0,0 +1,36 @@
+import os
+
+import pytest
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+BROWSER = os.environ.get('BROWSER', 'ChromeHeadless')
+
+
+@pytest.fixture(scope="module")
+def browser(request):
+    if BROWSER == 'ChromeHeadless':
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        browser = webdriver.Chrome(chrome_options=chrome_options)
+    else:
+        browser = getattr(webdriver, BROWSER)()
+    browser.implicitly_wait(3)
+    request.addfinalizer(lambda: browser.quit())
+    return browser
+
+
+@pytest.fixture(scope="module")
+def server_url(request, live_server):
+    if 'CUSTOM_SERVER_URL' in os.environ:
+        return os.environ['CUSTOM_SERVER_URL']
+    else:
+        return live_server.url
+
+
+def test_nav_menu(browser, server_url):
+    browser.get(server_url)
+
+    assert 'MENU' in browser.find_element_by_tag_name('body').text
+
