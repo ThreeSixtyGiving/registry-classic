7d136604767b011d146aff618e231816b9325e97
Ben Webb <bjwebb67@googlemail.com>
2018-06-18 12:47:24 +0100

    Add chrome --disable-dev-shm-usage option
    This is required to run in Travis' docker containers
    successfully.

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -16,6 +16,7 @@ def browser(request):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         browser = webdriver.Chrome(chrome_options=chrome_options)
     else:
         browser = getattr(webdriver, BROWSER)()
