04fc44bf9493d2225c750206826ca67aa534f832
Bibiana Cristofol Amat <bcristofol@gmail.com>
2018-09-13 15:07:54 +0100

    Filter by approved datasets

--- registry/salesforce.py
+++ registry/salesforce.py
@@ -3,12 +3,13 @@ from urllib.parse import urlparse
 
 from simple_salesforce import Salesforce
 
+
 sf = Salesforce(username='360support@opendataservices.coop', password='', security_token='')
 
 
 def clean_object(obj):
     new_obj = {}
-    # print(obj)
+
     obj.pop("attributes", None)
     for key, value in obj.items():
         new_key = key.replace("__c", "").replace("__r", "").replace("_", "")
@@ -21,12 +22,13 @@ def clean_output(api_output):
     datasets = []
 
     for dataset in api_output['records']:
-        clean_dataset = clean_object(dataset)
-        clean_dataset["license"] = clean_object(clean_dataset["license"])
-        clean_dataset["publisher"] = clean_object(clean_dataset["account"])
-        clean_dataset["publisher"]["domain"] = urlparse(clean_dataset["publisher"]["website"]).netloc
-        clean_dataset.pop("account")
-        datasets.append(clean_dataset)
+        if dataset['Approved__c']:
+            clean_dataset = clean_object(dataset)
+            clean_dataset["license"] = clean_object(clean_dataset["license"])
+            clean_dataset["publisher"] = clean_object(clean_dataset["account"])
+            clean_dataset["publisher"]["domain"] = urlparse(clean_dataset["publisher"]["website"]).netloc
+            clean_dataset.pop("account")
+            datasets.append(clean_dataset)
 
     return datasets
 
@@ -40,6 +42,9 @@ def get_allowed_domains(output):
 
 
 def get_salesforce_data():
-    output = clean_output(sf.query("SELECT Id, Name, License__r.Name, License__r.URL__c, Access_URL__c, Description__c, Download_URL__c, Account__r.Id, Account__r.Logo__c, Account__r.Name, Account__r.Website, Account__r.prefix__c, Date_First_Published__c, LastModifiedDate, Approved__c from Dataset__c ORDER BY Account__r.Name"))
+    sf_query = "SELECT Id, Name, License__r.Name, License__r.URL__c, Access_URL__c, Description__c, Download_URL__c," \
+            "Account__r.Id, Account__r.Logo__c, Account__r.Name, Account__r.Website, Account__r.prefix__c," \
+            "Date_First_Published__c, LastModifiedDate, Approved__c from Dataset__c ORDER BY Account__r.Name"
+    output = clean_output(sf.query(sf_query))
 
     return json.dumps(output, sort_keys=True, indent=2)
