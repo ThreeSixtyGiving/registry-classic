ed49fe241fbc1d266bc151f1a231781d95319807
Jared Parnell <jared.parnell@opendataservices.coop>
2021-05-10 12:00:30 +0100

    tester: Add working but ugly search box

--- registry/datastore.py
+++ registry/datastore.py
@@ -2,6 +2,6 @@ import requests
 
 DEV_DATASTORE_ENDPOINT = "http://store.dev.default.threesixtygiving.uk0.bigv.io/api/dashboard/publishers?format=json"
 
-def get_publisher_json():
-      return requests.get(DEV_DATASTORE_ENDPOINT).json()
+def get_publisher_json(query={}):
+      return requests.get(DEV_DATASTORE_ENDPOINT, params=query).json()
 
