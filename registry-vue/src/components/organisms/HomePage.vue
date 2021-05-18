35f9a135f190fbc4a90205c1e3a31462ee32e951
Jared Parnell <jared.parnell@opendataservices.coop>
2021-05-18 12:51:15 +0100

    vue: Downgrade to vue2

---
+++ registry-vue/src/components/organisms/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="layout layout--single-column">
+    <header class="layout__header">
+      <Hero />
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Hero from "../molecules/Hero"
+
+export default {
+  name: "HomePage",
+  components: {
+    Hero,
+  },
+};
+</script>
--- registry-vue/src/router/index.js
+++ registry-vue/src/router/index.js
@@ -1,12 +1,15 @@
-import { createWebHistory, createRouter } from "vue-router";
-import App from '../App.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import HomePageVue from '../components/organisms/HomePage';
 import DashboardPageVue from '../components/organisms/DashboardPage';
 import PublisherPageVue from '../components/organisms/PublisherPage';
+
+Vue.use(Router)
 
 const routes = [
   { path: '/', redirect: '/dashboard'},
   { path: '/', 
-    component: App,
+    component: HomePageVue,
     children: [
       { path: '/dashboard', name: "Dashboard", component: DashboardPageVue },
       { path: '/publishers', name: "Publishers", component: PublisherPageVue },
@@ -14,8 +17,7 @@ const routes = [
   },
   ]
 
-const router = createRouter({
-  history: createWebHistory(),
+const router = new Router({
   routes,
 });
 
