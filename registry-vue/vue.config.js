bc9aec5b1af8139ed264fb7e29216755b33ebe03
Jared Parnell <jared.parnell@opendataservices.coop>
2021-05-20 16:32:34 +0100

    data.json: Add route for data.json download
    Currently this is devserver specific middleware as specified in vue.config.js. If this is the route we take it should become production suitable middleware

--- registry-vue/vue.config.js
+++ registry-vue/vue.config.js
@@ -1,5 +1,18 @@
-// vue.config.js
+const fetch = require('node-fetch');
+
 module.exports = {
+  devServer: {
+    before: function(app, server) {
+      app.get('/data.json', async function(req, res) {
+        fetch("https://data.threesixtygiving.org/data.json", { method: "GET" })
+          .then(res => res.json())
+          .then((json) => {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(json));
+          });
+      });
+    }
+  },
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       // mutate config for production...
