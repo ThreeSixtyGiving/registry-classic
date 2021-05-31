5e1c917d834dbb4b9968a1be62b2921f640eb473
Jared Parnell <jared.parnell@opendataservices.coop>
2021-05-31 11:53:42 +0100

    publisher: Add publisher detail pages

---
+++ registry-vue/src/components/publishers/PublisherDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="layout__content">
+    <div class="layout__content-inner">
+      Publisher Detail
+    </div>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: "PublisherDetail",
+  components: {
+  },
+  methods: {
+  },
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+};
+</script>
