ba762e81952a5af25b623b16b464b4bf0f623108
Jared Parnell <jared.parnell@opendataservices.coop>
2021-06-11 16:22:13 +0100

    publisher-detail: Move insights button to file level, not publisher level

--- registry-vue/src/components/publishers/parts/FileSummary.vue
+++ registry-vue/src/components/publishers/parts/FileSummary.vue
@@ -1,31 +1,37 @@
 <template>
+<div >
+  <div class="grid__1 align-right dashboard-publisher-result-wrapper__external-buttons align-right">
+    <a href="#" target="_blank" class="button button--black">See in 360Insights</a>
+  </div>
   <div class="file-summary grid grid--three-columns">
-  <div class="grid__1">
-    {{ summary.description }}
+    <div class="grid__1">
+      {{ summary.description }}
+    </div>
+    <div class="grid__1 align-right">
+      <span class="file-summary__label">Records</span>
+      <span class="file-summary__stat">{{ summary.num_records }}</span>
+    </div>
+    <div class="grid__1 align-right">
+      <span class="file-summary__label">Total Grants &pound;</span>
+      <span class="file-summary__stat">{{ summary.grant_total_shorthand }}</span>
+    </div>
+    <div class="grid__1">
+      <span class="tag file-summary__specs">{{ summary.file_type }} &ndash; {{ summary.file_size }}</span>
+    </div>
+    <div class="grid__1 align-right">
+      <span class="file-summary__label">Valid data</span>
+      <MaterialIcon v-if="summary.validData" iconName='check_circle_outline' />
+      <MaterialIcon v-else iconName='priority_high' />
+    </div>
+    <div class="grid__1 align-right">
+      <span class="file-summary__label">Licence</span>
+      <span v-if="summary.licence === 'cc'" class="file-summary__native-icon">
+        <img src="@/assets/images/icons/cc.svg" alt="">
+      </span>
+      <span v-else class="file-summary__stat">{{ summary.licence }}</span>
+    </div>
   </div>
-  <div class="grid__1 align-right">
-    <span class="file-summary__label">Records</span>
-    <span class="file-summary__stat">{{ summary.num_records }}</span>
-  </div>
-  <div class="grid__1 align-right">
-    <span class="file-summary__label">Total Grants &pound;</span>
-    <span class="file-summary__stat">{{ summary.grant_total_shorthand }}</span>
-  </div>
-  <div class="grid__1">
-    <span class="tag file-summary__specs">{{ summary.file_type }} &ndash; {{ summary.file_size }}</span>
-  </div>
-  <div class="grid__1 align-right">
-    <span class="file-summary__label">Valid data</span>
-    <MaterialIcon v-if="summary.validData" iconName='check_circle_outline' />
-    <MaterialIcon v-else iconName='priority_high' />
-  </div>
-  <div class="grid__1 align-right">
-    <span class="file-summary__label">Licence</span>
-    <span v-if="summary.licence === 'cc'" class="file-summary__native-icon">
-      <img src="@/assets/images/icons/cc.svg" alt="">
-    </span>
-    <span v-else class="file-summary__stat">{{ summary.licence }}</span>
-  </div>
+  <hr class="separator-light">
 </div>
 </template>
 
@@ -39,6 +45,7 @@ export default {
   },
   props: {
     summary: {},
+    prefix: {},
   }
 }
 </script>
