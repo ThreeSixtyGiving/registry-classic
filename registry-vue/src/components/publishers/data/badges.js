7b350da69c0954f11da942f6cddc34cc4ad34a3c
Jared Parnell <jared.parnell@opendataservices.coop>
2021-06-14 15:39:20 +0100

    publishers: Get basic badges working

---
+++ registry-vue/src/components/publishers/data/badges.js
@@ -0,0 +1,41 @@
+const badges = [
+  {
+    qualityMetric: 'hasGrantDuration',
+    iconName: 'event_note',
+    label: 'Include grant duration'
+  },
+  {
+    qualityMetric: 'hasGrantClassification',
+    iconName: 'label',
+    label: 'Include classifications'
+  },
+  {
+    qualityMetric: 'hasGrantProgrammeTitle',
+    iconName: 'format_quote',
+    label: 'Include programme names'
+  },
+  {
+    qualityMetric: 'hasRecipientOrgLocations',
+    iconName: '',
+    label: 'Unknown Badge - Recipient Org Location'
+  },
+  {
+    qualityMetric: 'hasBeneficiaryLocationName',
+    iconName: 'edit_location',
+    label: 'Include grant location name'
+  },
+  {
+    qualityMetric: 'hasBeneficiaryLocationCodes',
+    iconName: 'location_on',
+    label: 'Include recipient location codes'
+  },
+]
+
+const getBadges = quality => {
+  const wonBadges = Object.keys(quality).filter(key => {
+    return quality[key] === 1 ? key : null;
+  });
+  return badges.filter(badge => wonBadges.includes(badge.qualityMetric))
+}
+
+export default getBadges;
