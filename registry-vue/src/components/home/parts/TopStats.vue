3ec93ef005ab81b35c3eac6f63d9ce83d01de402
Jared Parnell <jared.parnell@opendataservices.coop>
2021-06-15 12:08:51 +0100

    topstats: Make seperate API call for this data
    Consider refactoring this so that it gets the data passed from other components making the same calls

--- registry-vue/src/components/home/parts/TopStats.vue
+++ registry-vue/src/components/home/parts/TopStats.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="cards-section">
-    <section class="grid grid--four-columns">
+    <section class="grid grid--four-columns" :key="`ready-${dataDownloaded}`">
       <div class="base-card base-card--yellow grid__1">
         <div class="base-card__content">
-          <h2 class="base-card__title">589,282</h2>
+          <Spinner v-if="!dataDownloaded" />
+          <h2 v-if="dataDownloaded" class="base-card__title">{{ stats.grants }}</h2>
           <p class="base-card__text">Grants</p>
         </div>
       </div>
 
       <div class="base-card base-card--orange grid__1">
         <div class="base-card__content">
-          <h2 class="base-card__title">260,921</h2>
+          <Spinner v-if="!dataDownloaded" />
+          <h2 v-if="dataDownloaded" class="base-card__title">{{ stats.recipients }}</h2>
           <p class="base-card__text">Recipients</p>
         </div>
       </div>
 
       <div class="base-card base-card--teal grid__1">
         <div class="base-card__content">
-          <h2 class="base-card__title">172</h2>
+          <Spinner v-if="!dataDownloaded" />
+          <h2 v-if="dataDownloaded" class="base-card__title">Unknown</h2>
           <p class="base-card__text">Funders</p>
         </div>
       </div>
 
       <div class="base-card base-card--red grid__1">
         <div class="base-card__content">
-          <h2 class="base-card__title">£107.9bn</h2>
+          <Spinner v-if="!dataDownloaded" />
+          <h2 v-if="dataDownloaded" class="base-card__title">£{{ stats.GBP }}</h2>
           <p class="base-card__text">Total</p>
         </div>
       </div>
@@ -33,8 +37,45 @@
 </template>
 
 <script>
+import numeral from "numeral";
+import Spinner from '../../generic/Spinner'
+
 export default {
   name: "TopStats",
+  components: {
+    Spinner
+  },
+  data() {
+    return {
+      stats: {
+        grants: null,
+        recipients: null,
+        GBP: null,
+      },
+      dataDownloaded: false,
+    }
+  },
+  methods: {
+    getData() {
+      this.dataDownloaded = false;
+      console.log(this.$router.currentRoute)
+      const endpoint = this.$router.currentRoute.name === 'publisher' ? `/publisher/${this.$router.currentRoute.params.id}` : `/overview?mode=publishers`;
+      fetch(`${process.env.VUE_APP_DATASTORE_API}${endpoint}`)
+        .then((response) => response.json())
+        .then((json) => {
+          this.stats = json.aggregate.total;
+          const totalValue = numeral(json.aggregate.total.GBP);
+          this.stats.GBP = totalValue.format('( 0.00 a)');
+          this.dataDownloaded = true
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    },
+  },
+  created() {
+    this.getData();
+  }
 };
 </script>
 
