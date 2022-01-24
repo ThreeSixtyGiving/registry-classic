e0511e01d0c28b4ddfa4f00cca8aec6bf841554f
Michael Wood <michael.wood@opendataservices.coop>
2022-01-24 15:06:47 +0000

    registry/dashboard: Make the root url / configurable
    Switch between / being the dashboard or registry by using MODE=dashboard
    or blank/other value for registry

--- registry-vue/vue.config.js
+++ registry-vue/vue.config.js
@@ -2,5 +2,7 @@ var path = require('path');
 
 module.exports = {
   outputDir: process.env.NODE_ENV === 'production' ? '../registry/templates/vue-build/' : 'dist',
+  /* assetsDir is relative to output dir */
   assetsDir: process.env.NODE_ENV === 'production' ? '../../static/vue-build/' : '',
+  publicPath: process.env.MODE === 'dashboard' ? '/' : '/dashboard/',
 }
