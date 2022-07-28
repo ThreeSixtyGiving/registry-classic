4a0c8abd29a70bc2d2371456cfd31924a5bbad02
Jared Parnell <jared.parnell@opendataservices.coop>
2022-07-28 15:34:57 +0100

    awardYears: Create bandings for heatmap

--- registry-vue/src/components/generic/YearsChart.vue
+++ registry-vue/src/components/generic/YearsChart.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="years-chart">
-    <li v-for="(item, index) in chart" :key="`item-${index}`" class="years-chart__item" :style="`--opacity: ${item.value / 100};`">
+    <li v-for="(item, index) in chart" :key="`item-${index}`" class="years-chart__item" :style="`--opacity: ${calculateOpacity(item.value)};`">
       <div class="years-chart__block" :aria-label="item.detail" :title="item.detail">
         <span>{{ item.value_text }}</span>
       </div>
@@ -14,6 +14,40 @@ export default {
   name: "YearsChart",
   props: {
     chart: {},
-  }
+  },
+  data: function() {
+    return {
+      grantBandings: [0, 25, 50, 100, 250, 500, 1000, 5000],
+    };
+  },
+  methods: {
+    calculateOpacity(value){
+      if (value == this.grantBandings[0] || typeof value == "undefined" || value == null){
+        return 0;
+      }
+      if (value < this.grantBandings[1]){
+        return 1 / this.grantBandings.length;
+      }
+      if (value < this.grantBandings[2]){
+        return 2 / this.grantBandings.length;
+      }
+      if (value < this.grantBandings[3]){
+        return 3 / this.grantBandings.length;
+      }
+      if (value < this.grantBandings[4]){
+        return 4 / this.grantBandings.length;
+      }
+      if (value < this.grantBandings[5]){
+        return 5 / this.grantBandings.length;
+      }
+      if (value < this.grantBandings[6]){
+        return 6 / this.grantBandings.length;
+      }
+      if (value < this.grantBandings[7]){
+        return 7 / this.grantBandings.length;
+      }
+      return 1;
+    },
+  },
 }
 </script>
