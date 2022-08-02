57e9e132b5d247d58253e542c91eab5eb072ec93
Jared Parnell <jared.parnell@opendataservices.coop>
2022-08-02 09:32:25 +0100

    awardYears: Refactor and simplify

--- registry-vue/src/components/generic/YearsChart.vue
+++ registry-vue/src/components/generic/YearsChart.vue
@@ -1,7 +1,16 @@
 <template>
   <ul class="years-chart">
-    <li v-for="(item, index) in chart" :key="`item-${index}`" class="years-chart__item" :style="`--opacity: ${calculateOpacity(item.value)};`">
-      <div class="years-chart__block" :aria-label="item.detail" :title="item.detail">
+    <li
+      v-for="(item, index) in chart"
+      :key="`item-${index}`"
+      class="years-chart__item"
+      :style="`--opacity: ${calculateOpacity(item.value)};`"
+    >
+      <div
+        class="years-chart__block"
+        :aria-label="item.detail"
+        :title="item.detail"
+      >
         <span>{{ item.value_text }}</span>
       </div>
       <div class="years-chart__label">{{ item.label }}</div>
@@ -22,29 +31,14 @@ export default {
   },
   methods: {
     calculateOpacity(value){
-      if (value == this.grantBandings[0] || typeof value == "undefined" || value == null){
-        return 0;
-      }
-      if (value < this.grantBandings[1]){
-        return 1 / this.grantBandings.length;
-      }
-      if (value < this.grantBandings[2]){
-        return 2 / this.grantBandings.length;
-      }
-      if (value < this.grantBandings[3]){
-        return 3 / this.grantBandings.length;
-      }
-      if (value < this.grantBandings[4]){
-        return 4 / this.grantBandings.length;
-      }
-      if (value < this.grantBandings[5]){
-        return 5 / this.grantBandings.length;
-      }
-      if (value < this.grantBandings[6]){
-        return 6 / this.grantBandings.length;
-      }
-      if (value < this.grantBandings[7]){
-        return 7 / this.grantBandings.length;
+      const bandingsLength = this.grantBandings.length;
+
+      for (let i = 0; i < bandingsLength; i++) {
+        if (value == 0 || typeof value == "undefined" || value == null) {
+          return 0;
+        } else if (value < this.grantBandings[i]) {
+          return (i / bandingsLength);
+        }
       }
       return 1;
     },
