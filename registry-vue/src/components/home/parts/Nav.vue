3ee9dcd3d21b06cb49850d9adc71076559e8f8d7
Myles Lewando <myles.lewando@opendataservices.coop>
2023-05-11 08:36:31 +0100

    frontend: Update header

--- registry-vue/src/components/home/parts/Nav.vue
+++ registry-vue/src/components/home/parts/Nav.vue
@@ -1,17 +1,35 @@
 <template>
-    <div class="nav-bar nav-bar--minimal">
-        <a class="nav-bar__home-button" href="/">
-            <img src="@/assets/images/360-logos/360dataqualitydashboard-color.svg" alt="360DataQualityDashboard" style="width: unset; height:94px">
-        </a>
-
-        <nav class="nav-bar__nav-menu">
-            <ul>
-                <li class="nav-menu__item">
-                    Based on data published by UK grantmakers in the 360Giving Data Standard
-                </li>
-            </ul>
-        </nav>
+  <div>
+    <div class="top-bar">
+      <nav class="top-bar__menu contextual-menu">
+        <ul>
+          <li class="contextual-menu__item">
+            <a class="contextual-menu__button" href="https://insights.threesixtygiving.org/" title="Insights">Insights</a>
+          </li>
+          <li class="contextual-menu__item">
+            <a class="contextual-menu__button" href="https://threesixtygiving.org/about/" title="About">About</a>
+          </li>
+          <li class="contextual-menu__item">
+            <a class="contextual-menu__button" href="https://grantnav.threesixtygiving.org/" title="GrantNav">GrantNav</a>
+          </li>
+        </ul>
+      </nav>
     </div>
+
+    <div class="nav-bar nav-bar--minimal">
+      <a class="nav-bar__home-button" href="/">
+        <img src="@/assets/images/360-logos/360dataqualitydashboard-color.svg" alt="360DataQualityDashboard" style="width: unset; height:94px">
+      </a>
+
+      <nav class="nav-bar__nav-menu">
+        <ul>
+          <li class="nav-menu__item">
+            Based on data published by UK grantmakers in the 360Giving Data Standard
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 </template>
 
 <script>
